0818ab91c376e00ba594889c60fa0d65ad0dd9f0
SuJith-154 <sujipjk03@gmail.com>
2026-06-14 22:37:18 +0530

    site map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://jithx.vercel.app";
+  const baseUrl = "https://jith-x.vercel.app";
 
   return [
     {
